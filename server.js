21c80c39a5c09184878aaaa97c4fa9f5ca204726
Alex <suhobski@mail.ru>
2022-02-06 22:15:49 +0300

    feat: add code

---
+++ server.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+const path = require("path");
+const https = require("https");
+
+const express = require("express");
+const helmet = require("helmet`");
+
+const PORT = 3000;
+
+const app = express();
+
+app.use(helmet());
+
+app.get("/secret", (req, res) => {
+  return res.send("Your secret value is 42!");
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+https
+  .createServer(
+    {
+      key: fs.readFileSync("key.pem"),
+      cert: fs.readFileSync("req.pem"),
+    },
+    app
+  )
+  .listen(PORT, () => {
+    console.log(`Listeting on port ${PORT}...`);
+  });
